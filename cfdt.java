e8c21e4f5e079fda17c112889908f452e2060f98
Jatin Rawat <101701244+jatinrawat3110@users.noreply.github.com>
2024-05-05 22:10:20 +0530

    code commit initial
    code commit initial

---
+++ cfdt.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+public class cfdt {
+    public static void main(String[] args) {
+        Date cdt= new Date(2024,3,22);
+        System.out.println(cdt.getDay());
+        System.out.println("Current full date and time is "+cdt);
+    }
+}
